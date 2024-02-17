c2891d3893a5cc0d4df2fb795697809a46b89030
James O'Toole <otoolej012+gh@gmail.com>
2024-02-17 17:18:17 +0000

    Add ask_once function for asking a question to GPT-3.5-turbo

--- gptbatcher/GPTBatcher.py
+++ gptbatcher/GPTBatcher.py
@@ -1,8 +1,68 @@
+import json
 from openai import OpenAI
 from pandas import DataFrame
 from typing import List, Dict
 from gptbatcher.Question import Question
 from gptbatcher.Participant import Participant
+from gptbatcher.Choice import Choice
+
+
+def ask_once(openai: OpenAI, question: Question, participant: Participant) -> Choice:
+    completion = openai.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": participant.prompt},
+            {"role": "user", "content": question.prompt},
+        ],
+        tools=[{
+            "type": "function",
+            "function": {
+                "name": "vote",
+                "description": "Vote on a poll",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        **{
+                            choice.label: {
+                                "type": "boolean",
+                                "description": choice.prompt
+                            } for choice in question.choices
+                        }
+                    },
+                    "required": [choice.label for choice in question.choices],
+                }
+            }
+        }],
+        tool_choice={
+            "type": "function",
+            "function": {
+                "name": "vote",
+            }
+        }
+    )
+
+    choice = completion.choices[0]
+    if choice.finish_reason is not "tool_calls":
+        raise Exception("Tool call not made")
+    
+    calls = choice.message.tool_calls
+    if not calls or len(calls) != 1:
+        raise Exception("Expected one tool call")
+
+    json_string = calls[0].function.arguments
+    results = json.loads(json_string)
+
+    chosen = None
+    for choice in question.choices:
+        if results[choice.label]:
+            if chosen:
+                raise Exception("Multiple choices selected")
+            chosen = choice
+
+    if not chosen:
+        raise Exception("No choice selected")
+    
+    return chosen
 
 
 class GPTBatcher:
@@ -10,5 +70,4 @@ class GPTBatcher:
         self.openai = OpenAI(api_key=api_key)
 
     def ask(self, question: Question, participants: List[Participant]) -> DataFrame:
-        pass
-
+        raise NotImplementedError
