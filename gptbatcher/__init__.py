8a6d0b2e79bc1e9d3bab9aee225730a881946510
James O'Toole <otoolej012+gh@gmail.com>
2024-02-20 12:29:29 +0000

    still trying to fix ci

--- gptbatcher/__init__.py
+++ gptbatcher/__init__.py
@@ -2,3 +2,5 @@ from .GPTBatcher import GPTBatcher
 from .Question import Question
 from .Choice import Choice
 from .Participant import Participant
+
+__all__ = ["GPTBatcher", "Question", "Choice", "Participant"]
