9f036c6db9332ed4fa2a655abbe08b2daf770178
James O'Toole <otoolej012+gh@gmail.com>
2024-02-20 12:57:09 +0000

    Update version and add long description to setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='gptbatcher',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/CBM-Digital/GPTBatcher',
     author="James O'Toole",
     author_email='jamesotoole@cbmdigital.co.uk',
     description='Conduct surveys using GPT',
+    long_description=open('README.md').read(),
     packages=find_packages(),    
     install_requires=[
         'openai>=1.12.0',
