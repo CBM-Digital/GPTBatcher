be72621d80a1bfaf4ba07b63172b34f5f3e7854e
James O'Toole <otoolej012+gh@gmail.com>
2024-02-22 10:27:12 +0000

    Update version number in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gptbatcher',
-    version='0.1.4',
+    version='0.2.0',
     url='https://github.com/CBM-Digital/GPTBatcher',
     author="James O'Toole",
     author_email='jamesotoole@cbmdigital.co.uk',
